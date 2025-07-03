0f9c1b27f67142a324cc5731cd093a2e1c56c353
Deilons <apolo4748@email.com>
2025-07-03 18:37:53 -0500

    Feat: Add DTOs for categories and inventory movements

---
+++ PolimericosInventoryAPI/DTOs/InventoryMovementCreateDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PolimericosInventoryAPI.DTOs
+{
+    public class InventoryMovementCreateDTO
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string MovementType { get; set; } // Entry o Exit
+
+        [Required]
+        public decimal Quantity { get; set; }
+
+        [MaxLength(200)]
+        public string? Note { get; set; }
+    }
+}
---
+++ PolimericosInventoryAPI/DTOs/InventoryMovementDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace PolimericosInventoryAPI.DTOs
+{
+    public class InventoryMovementDTO
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string MovementType { get; set; } // "Entry" o "Exit"
+        public decimal Quantity { get; set; }
+        public DateTime Date { get; set; }
+        public string? Note { get; set; }
+    }
+}
