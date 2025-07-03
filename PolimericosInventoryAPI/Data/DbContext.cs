82e5cc79890f66ba8143c282e8d38f1998042511
Deilons <apolo4748@email.com>
2025-07-03 13:45:38 -0500

    Feat: product model

--- PolimericosInventoryAPI/Data/DbContext.cs
+++ PolimericosInventoryAPI/Data/DbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PolimericosInventoryAPI.Models;
 
 namespace PolimericosInventoryAPI.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
     }
 }
